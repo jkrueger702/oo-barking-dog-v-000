db2bf59bdb2e01da567d339adf5badc945fd675b
Jordan Krueger <jkrueger702@gmail.com>
2018-06-25 01:30:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
+class Dog
+
+  def initialize(name)
+    @name = name
+  
+end
+
+  def name=(name)
+    @name = name
+  end
+
+def name
+    @name
+  end
+
+class Dog
+
+def bark
+ puts "Woof!"  
+  
+end
 # Your code goes here!
